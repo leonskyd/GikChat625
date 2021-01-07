b1da9f50e8e0edc2450a599069b0d14abe4d6189
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-01-07 10:07:19 +0200

    Update Start.java
    Exceptions related to DB implementations have been added

--- IdeaProjects/chat/server/src/main/java/server/Start.java
+++ IdeaProjects/chat/server/src/main/java/server/Start.java
@@ -2,6 +2,12 @@ package server;
 
 public class Start {
     public static void main(String[] args) {
-        new Server();
+        try {
+            new Server();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
